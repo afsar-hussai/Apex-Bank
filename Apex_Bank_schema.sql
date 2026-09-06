2782ce5edb6f7024d27be8ccff03079679760fc4
afsar-hussai <afsarhussain2200@gmail.com>
2026-09-06 18:32:44 +0530

    sql file changes

--- Apex_Bank_schema.sql
+++ Apex_Bank_schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "users" (
-  "id" uuid PRIMARY KEY DEFAULT (gen_random_uuid()),
+  "id" uuid PRIMARY KEY NOT NULL DEFAULT (gen_random_uuid()),
   "full_name" varchar(100) NOT NULL,
   "email" varchar(255) UNIQUE NOT NULL,
   "phone" varchar(20) UNIQUE NOT NULL,
@@ -12,17 +12,17 @@ CREATE TABLE "users" (
 );
 
 CREATE TABLE "accounts" (
-  "id" bigserial PRIMARY KEY,
+  "id" bigserial PRIMARY KEY NOT NULL,
   "user_id" uuid NOT NULL,
   "balance" bigint NOT NULL DEFAULT 0,
   "currency" varchar(3) NOT NULL,
   "account_type" varchar NOT NULL,
-  "created_at" timestamptz DEFAULT (now()),
+  "created_at" timestamptz NOT NULL DEFAULT (now()),
   "updated_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "transactions" (
-  "id" uuid PRIMARY KEY DEFAULT (gen_random_uuid()),
+  "id" uuid PRIMARY KEY NOT NULL DEFAULT (gen_random_uuid()),
   "sender_account_id" bigint,
   "receiver_account_id" bigint,
   "amount" bigint NOT NULL,
@@ -31,28 +31,28 @@ CREATE TABLE "transactions" (
   "transaction_type" varchar NOT NULL,
   "idempotency_key" varchar(255) UNIQUE,
   "description" text,
-  "created_at" timestamptz DEFAULT (now())
+  "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "ledger_entries" (
-  "id" bigserial PRIMARY KEY,
+  "id" bigserial PRIMARY KEY NOT NULL,
   "account_id" bigint NOT NULL,
   "transaction_id" uuid NOT NULL,
   "credit_debit" varchar NOT NULL,
   "amount" bigint NOT NULL,
   "balance_after" bigint NOT NULL,
-  "created_at" timestamptz DEFAULT (now())
+  "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "audit_logs" (
-  "id" bigserial PRIMARY KEY,
+  "id" bigserial PRIMARY KEY NOT NULL,
   "actor_id" uuid,
   "action" varchar NOT NULL,
   "table_name" varchar NOT NULL,
   "old_values" jsonb,
   "new_values" jsonb,
   "ip_address" varchar,
-  "created_at" timestamptz DEFAULT (now())
+  "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 ALTER TABLE "accounts" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id") DEFERRABLE INITIALLY IMMEDIATE;
